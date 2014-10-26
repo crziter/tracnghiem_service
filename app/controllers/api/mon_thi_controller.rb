dd29afaff9a1f402eeb2ed021042fa7bcbb6693e
Bình Nguyên <crziter@gmail.com>
2014-10-27 01:29:58 +0700

    Chức năng get all / insert đã hoạt động

--- app/controllers/api/mon_thi_controller.rb
+++ app/controllers/api/mon_thi_controller.rb
@@ -1,4 +1,6 @@
 class Api::MonThiController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     @mon_thi = MonThi.all
 
@@ -9,6 +11,30 @@ class Api::MonThiController < ApplicationController
   end
 
   def create
-    
+    begin
+      information = request.raw_post
+      mon_thi = JSON.parse(information)
+      ten_mon_thi = mon_thi['ten']
+
+      if ten_mon_thi != nil
+        mt = MonThi.new
+        mt.ten=ten_mon_thi
+        mt.save
+
+        return_obj :status => 'Ok', :inserted_id => mt.id
+      else
+        return_obj :status => 'Invalid MonThi object'
+      end
+    rescue
+      return_obj :status => 'Invalid json'
+    end
+  end
+
+  private
+  def return_obj(obj)
+    respond_to do |format|
+      format.json { render json: obj }
+      format.json { render json: obj }
+    end
   end
 end
